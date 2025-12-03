2f41f0818613bbb9342ede427d6a9b83ccdf1322
pedrorribeiro6 <pedro.r.ribeiro6@aluno.senai.br>
2025-12-03 16:49:52 -0300

    aula dia 3/12
    realizamos esse arquivo php no dia 3/12

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'config.php';
+
+//Verificar se o usúario está logado
+
+if (!isset($_SESSION['usuario_id'])){
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Financeiro</title>
+</head>
+<body>
+    <h1>Sistema Financeiro</h1>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'config.php';
+//Verificar se o usúario já está logado
+
+if (isset($_SESSION['usuario_id'])){
+    header('Location: index.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistema Financeiro</title>
+</head>
+<body>
+    <h1>Login - Sistema Financeiro</h1>
+    <form action="autenticar.php" method="post">
+        <div>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" required> 
+        </div>
+        <div>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha" required> 
+        </div>
+        <div>
+            <button type="submit">Entrar</button>
+        </div>
+    </form>
+
+    <p>Não tem conta?<a href="registro.php">Cadastre-se aqui.</a></p>
+</body>
+</html>
